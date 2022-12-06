9057ff6436e7c9af087eef0ecbbdd6a176eebb96
unknown <vanshbaghel912@gmail.com>
2022-12-07 00:16:58 +0530

    made login page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginPage from './Login/LoginPage';
+import Home from './Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+            <Router>
+            <Routes>
+            <Route index path='/' element={<Home />}></Route>
+                <Route index path='/Login' element={<LoginPage />}></Route>
+                <Route path="/*" element={<h1>Error Page</h1>}></Route>
+            </Routes>
+        </Router>
     </div>
   );
 }
---
+++ src/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import LoginButton from '../Login/LoginButton'
+
+const Home = () => {
+  return (
+    <div>
+        <LoginButton />
+    </div>
+  )
+}
+
+export default Home;
